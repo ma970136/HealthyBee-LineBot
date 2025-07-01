5cc9040cb88f8320a8985194e2d9a4389fa764c7
Cindy <ma970136@gmail.com>
2025-07-01 23:53:06 +0800

    test today

--- app.py
+++ app.py
@@ -321,6 +321,7 @@ def handle_message(event):
         seven_days_ago = today - timedelta(days=6)
         response = requests.get(thingspeak_url)
         feeds = response.json().get("feeds", [])
+        
         # 每天的最後一筆步數
         daily_data = {}
         for feed in reversed(feeds):  # 從最新的資料找
@@ -330,14 +331,20 @@ def handle_message(event):
                 try:
                     ts = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ") + timedelta(hours=8)
                     date = ts.date()
-                    # result = f"🟡 取得資料：{created_at} → 台灣時間：{ts} → 日期：{date} → 步數：{val}"
-                    result = str(ts)
+
                     if seven_days_ago <= date <= today:
                         if date not in daily_data:
                             daily_data[date] = int(float(val))
                 except Exception:
                     continue
+        
+        if today in daily_data:
+            result = f"今天步數是：{daily_data[today]} 步"
+        else:
+            result = f"今天還沒有步數資料。"
+
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=result))
+
 
 
     # ✅ 查心率指令
